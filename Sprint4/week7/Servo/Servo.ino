ed073e06fcbef868faa994bb8f626926f8ed9063
Metehan Topaç <metehan.topac02@gmail.com>
2022-06-11 17:30:36 +0200

    servo code update

--- Sprint4/week7/Servo/Servo.ino
+++ Sprint4/week7/Servo/Servo.ino
@@ -2,52 +2,82 @@
 
 Servo servoBase;
 Servo servoArmY;
-Servo servoGripper;
 
 const int sensor1Pin = A1;
 const int sensor2Pin = A2;
 const int sensor3Pin = A3;
+const int baseSwitch = A4;
 
 //those value need to be filled 
 int bufferBaseAngle = 30;
 int sliderBaseAngle = 120;
 int pickUpArmYAngle = 0;
 int dropArmYAngle = 0;
-int gripperCloseAngle = 0;
-int gripperOpenAngle = 0;
+
+bool controlVal = true;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensor1Pin, INPUT);
   pinMode(sensor2Pin, INPUT);
   pinMode(sensor3Pin, INPUT);
+  pinMode(baseSwitch, INPUT);
   servoBase.attach(7);
   servoArmY.attach(8);
-  servoGripper.attach(9);
   MoveToDefaultPosition();
 
 }
 
 void loop() {
   int sensorPusher = analogRead(sensor1Pin); //value from the optical sensor
-  PickUpAndDropDisk();
+  int baseStopper = analogRead(baseSwitch);
 
 }
 
-void runServo(int val){
-  servo.attach(Servo_PWM);
+void moveServo(Servo servo, int val, int servoPin){
+  servo.attach(servoPin);
   servo.write(val);
   delay(1000);
   servo.detach();
   delay(500);
   }
 
+  void switchServo(){
+  if(controlVal == true){
+    val = analogRead(baseSwitch);
+    }
+  Serial.println(val);
+  delay(50);
+  if(val > 5){
+    servoBase.writeMicroseconds(1500);
+    controlVal = false;
+  }else if (val == 0){
+    servoBase.writeMicroseconds(2000);
+    delay(50);
+    switchServo();
+  } 
+  }
+
+void moveBaseServo(int howLong, String where){
+  if(where == "clockwise"){
+    servoBase.writeMicroseconds(2000);
+    delay(howLong);
+    servoBase.writeMicroseconds(1500);
+    delay(500);
+  } else if(where == "counter-clockwise"){
+    servoBase.writeMicroseconds(1000);
+    delay(howLong);
+    servoBase.writeMicroseconds(1500);
+    delay(500);
+    }
+  
+  }
+
 void MoveToDefaultPosition(){
-  servoBase.write(0);
+  
+  moveBaseServo(1000, "counter-clockwise");
   delay(300);
-  servoArmY.write(0);
-  delay(300);
-  servoGripper.write(0);
+  moveServo(servoArmY, 0, 8);
   delay(300);
   }
 
@@ -55,16 +85,27 @@ void PickUpAndDropDisk(){
   //Picks up the disk from buffer zone
   servoGripper.write(gripperOpenAngle);
   delay(300);
-  servoBase.write(bufferBaseAngle);
+  
+  for(int i = 0; i < 1000; i++){ // when baseStopper sensor is detected the mark, the base will stop there
+    servoBase.writeMicroseconds(2000); //run
+    delay(50);
+    if(baseSwitch > 0){
+      servoBase.writeMicroseconds(1000); //stop
+      break;
+    }
+  }
+  
   delay(500);
-  for(int i = 0; i <= pickUpArmYAngle; i++)
+  for(int i = 0; i <= pickUpArmYAngle; i++) // puts the arm down
   {
     servoArmY.write(i);
     delay(30);
   }
   delay(500);
-  servoGripper.write(gripperCloseAngle);
-  for(int i = pickUpArmYAngle; i >= 0; i--)
+  
+  servoGripper.write(gripperCloseAngle); // closes the gripper
+  
+  for(int i = pickUpArmYAngle; i >= 0; i--) // puts the arm up
   {
     servoArmY.write(i);
     delay(30);
@@ -72,16 +113,18 @@ void PickUpAndDropDisk(){
   delay(500);
   
   //Drops the disk to slider
-  servoBase.write(sliderBaseAngle);
+  moveBaseServo(1000, "clockwise"); // rotates to slider
   delay(500);
-  for(int i = 0; i >= dropArmYAngle; i++)
+  
+  for(int i = 0; i >= dropArmYAngle; i++) // puts the arm down
   {
     servoArmY.write(i);
     delay(30);
   }
   delay(300);
-  servoGripper.write(gripperOpenAngle);
+  servoGripper.write(gripperOpenAngle); // opens the gripper and drops the disk
   delay(300);
+  
   MoveToDefaultPosition();
   
   }
